235035efe583e9aea874d6b7f4ac018a6cd0d681
Jaz <jasminemaslen7@gmail.com>
2024-09-20 17:39:43 +0100

    Added select element functionality

--- src/script.js
+++ src/script.js
@@ -1,12 +1,30 @@
-function updateTime(timezone) {
-	let date = document.querySelector("#date");
-	let time = document.querySelector("#time");
+let selectCity = document.querySelector("#select-menu");
+selectCity.addEventListener("change", getTimezone);
 
-	date.innerHTML = moment().tz(timezone).format("dddd Do MMMM");
-	time.innerHTML = moment().tz(timezone).format("HH:mm:ss");
+function getTimezone(event) {
+	timezone = selectCity.value;
+	updateTime(timezone);
 }
 
-updateTime("Europe/london");
+function updateTime(timezone) {
+	let dateElement = document.querySelector("#date");
+	let timeElement = document.querySelector("#time");
+
+	let cityElement = document.querySelector("#city");
+	let city = timezone.split("/")[1];
+
+	function capitalizeCity(string) {
+		return string[0].toUpperCase() + string.slice(1);
+	}
+
+	dateElement.innerHTML = moment().tz(timezone).format("dddd Do MMMM");
+	timeElement.innerHTML = moment().tz(timezone).format("HH:mm:ss");
+	cityElement.innerHTML = capitalizeCity(city);
+}
+
+let timezone = "Europe/london";
+updateTime(timezone);
+
 let intervalId = setInterval(() => {
-	updateTime("Europe/london");
+	updateTime(timezone);
 }, 1000);
